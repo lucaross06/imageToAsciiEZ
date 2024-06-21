65ec4fbb78a5e7949905a3eec578a771e55d343a
lucaross06 <lucarossin06@gmail.com>
2024-06-21 11:34:14 +0200

    modified:   input.txt 	modified:   main.c 	new file:   main.exe

--- main.c
+++ main.c
@@ -4,9 +4,60 @@
 
 int main(){
     char c;
-
-    FILE *f = fopen("input.txt",'r');
-    
+    int width = 10;
+    int height = 10;
+    int x,y = 0;
+    FILE *f = fopen("input.txt","r");
+    if(f == NULL){
+        perror("main");
+        exit(EXIT_FAILURE);
+    }
+    else{
+        while((c = fgetc(f)) != EOF){
+            switch (c){
+                case '0':
+                    printf(" ");
+                    break;
+                case '1':
+                    printf(".");
+                    break;
+                case '2':
+                    printf(":");
+                    break;
+                case '3':
+                    printf("l");
+                    break;
+                case '4':
+                    printf("/");
+                    break;
+                case '5':
+                    printf("c");
+                    break;
+                case '6':
+                    printf("a");
+                    break;
+                case '7':
+                    printf("d");
+                    break;
+                case '8':
+                    printf("8");
+                    break;
+                case '9':
+                    printf("@");
+                    break;
+                case '\n':
+                    break;
+            };
+            if(x == width){
+                printf("\n");
+                y++;
+                x = 0;
+            }
+            else{
+                x++;
+            }
+        }
+    }
 
 
 }
